e55e0468b2b63c485d94b34f92679f3788e0fcbd
WeiChen Lin <asdfg09487@gmail.com>
2022-11-26 15:10:19 +0800

    feat(index): add to home page

--- layouts/sidebar.tsx
+++ layouts/sidebar.tsx
@@ -1,5 +1,5 @@
 import Menu from 'components/layout'
-
+import router from 'next/router'
 interface Props {
   currentSite: string
 }
@@ -7,8 +7,13 @@ interface Props {
 export default function SideBar(props: Props) {
   const { currentSite } = props
   return (
-    <div className='w-1/5 border-r-2 border-zinc-400/20 flex flex-col justify-between px-10 py-12'>
-      <h1 className='text-3xl text-gray-100 font-bold opacity-60'>
+    <div className='w-1/5 border-r-2 border-zinc-400/20 flex flex-col justify-between px-10 py-12 cur'>
+      <h1
+        className='text-3xl text-gray-100 font-bold opacity-60 cursor-pointer'
+        onClick={() => {
+          router.push('/')
+        }}
+      >
         WeiChen Lin<span className='text-5xl text-red-200'>.</span>
       </h1>
       <Menu currentSite={currentSite}></Menu>
